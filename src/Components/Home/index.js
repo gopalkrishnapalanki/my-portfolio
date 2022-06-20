a78e1135c0845992c46d9c646243217bbcc97aae
gopalkrishnapalanki <gopirevs27@gmail.com>
2022-06-20 12:53:35 +0530

    title change

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,26 +1,21 @@
-import React from 'react'
-import "./index.css"
-import Header from '../Header'
-import Cookies from 'js-cookie'
-import { Redirect, Link } from 'react-router-dom'
-import Skills from '../Skills'
-import Footer from '../Footer'
-import Portfolio from '../Portfolio'
-import Slider from '../Slider'
+import React from "react";
+import "./index.css";
+import Header from "../Header";
+import Cookies from "js-cookie";
+import Skills from "../Skills";
+import Portfolio from "../Portfolio";
+import Slider from "../Slider";
 const Home = () => {
- 
-  
   return (
     <>
-    <div>
-      <Header />
-     <Portfolio/>
-   
+      <div>
+        <Header />
+        <Portfolio />
         <Skills />
-        <Slider/>
-        </div>
-      </>
-  )
-}
+        <Slider />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
